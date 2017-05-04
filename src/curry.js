6c4d3e75bad2a4ae1bd1f05469e7c9d162fad19e
rs222kn <rs222kn@student.lnu.se>
2017-05-04 23:13:55 +0200

    simple curry

---
+++ src/curry.js
@@ -0,0 +1,24 @@
+/**
+ * Simple curry
+ * @param {Function} fn - function to curry
+ * @param {Array} curryArgs - pre defined param
+ * @return {Function} _ - function to take rest of args
+ * @param {Array} args - params for curried function
+ * @return {Any||Function} _ - returns result of fn or function to take rest of args
+ * @example 
+ * 
+ * const add = (a, b) => a + b
+ * const addC = curry(add)
+ * 
+ * add(1)(1)
+ * add(1, 1)
+ */
+const curry = (fn, curryArgs = []) => (...args) => {
+  const concatArgs = [...curryArgs, ...args]
+
+  return fn.length > concatArgs.length ? 
+    curry(fn, concatArgs) : 
+    fn(...concatArgs)
+}
+
+module.exports = curry
