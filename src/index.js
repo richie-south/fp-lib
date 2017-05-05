fb3a431b18dc5d4262670ff612ad40a775d8d8b3
rs222kn <rs222kn@student.lnu.se>
2017-05-05 11:44:20 +0200

    more exports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 const curry = require('./curry')
 const compose = require('./compose')
-
+const map = require('./map')
+const filter = require('./filter')
+const { isArray, isObject, isString } = require('./isType')
 
 module.exports = {
   curry,
   compose,
+  map,
+  filter,
+  
+  isArray, 
+  isObject, 
+  isString,
 }
