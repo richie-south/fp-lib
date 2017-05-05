8f08cd902c80ee210eb314fbf2cba07b1fd3d249
rs222kn <rs222kn@student.lnu.se>
2017-05-05 13:30:38 +0200

    added reduce to export object

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const curry = require('./curry')
 const compose = require('./compose')
 const map = require('./map')
 const filter = require('./filter')
+const reduce = require('./reduce')
 const { isArray, isObject, isString } = require('./isType')
 
 module.exports = {
@@ -9,6 +10,7 @@ module.exports = {
   compose,
   map,
   filter,
+  reduce,
   
   isArray, 
   isObject, 
