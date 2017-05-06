5313cd3437830fad049bc0b880b3fc6d793e7175
rs222kn <rs222kn@student.lnu.se>
2017-05-06 16:51:31 +0200

    simple chain test

---
+++ test/chain.js
@@ -0,0 +1,30 @@
+const chai = require('chai')
+const expect = chai.expect
+const assert = chai.assert
+
+const chain = require('../src/chain')
+
+const duplicate = n => [n, n]
+
+describe('chain', () => {
+
+  it('should return duplicated value array', () => {
+    const expected = [1, 1, 2, 2, 3, 3]
+    const testArray = [1, 2, 3]
+    
+    const result = chain(duplicate, testArray)
+
+    expect(result).to.deep.equal(expected)
+  })
+
+  it('should be curry:ed', () => {
+    const expected = [1, 1]
+    const testArray = [1]
+    const duplicateValues = chain(duplicate)
+    const result = duplicateValues(testArray)
+
+    assert.isFunction(duplicateValues)
+    expect(result).to.deep.equal(expected)
+  })
+
+})
