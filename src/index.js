ddcad1b221d106ecd98f1b727ee05fc4c4ff22aa
rs222kn <rs222kn@student.lnu.se>
2017-05-06 22:21:14 +0200

    messed to export chain and always ...

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const compose = require('./compose')
 const map = require('./map')
 const filter = require('./filter')
 const reduce = require('./reduce')
+const chain = require('./chain')
+const always = require('./always')
 const { isArray, isObject, isString } = require('./isType')
 
 module.exports = {
@@ -11,6 +13,8 @@ module.exports = {
   map,
   filter,
   reduce,
+  chain,
+  always,
   
   isArray, 
   isObject, 
