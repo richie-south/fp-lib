87b8ce85042ef3953c992ebc5bf8e0389cf0ddb9
rs222kn <rs222kn@student.lnu.se>
2017-05-11 13:21:39 +0200

    Simple pipe

--- test/pipe.js
+++ test/pipe.js
@@ -2,37 +2,37 @@ const chai = require('chai')
 const expect = chai.expect
 const assert = chai.assert
 
-const compose = require('../src/compose')
-
+const pipe = require('../src/compose').pipe
+console.log(pipe)
 const timeTwo = a => a * 2
 const minusOne = a => a - 1
 const addTwo = a => a + 2
 
-describe('compose', () => {
+describe('pipe', () => {
 
   it('should return function', () => {
-    const result = compose(timeTwo, minusOne)
+    const result = pipe(timeTwo, minusOne)
 
     assert.isFunction(result)
   })
 
-  it('should run right to left', () => {
-    const expected = 2
-    const result = compose(timeTwo, minusOne)(2)
+  it('should run left to right', () => {
+    const expected = 3
+    const result = pipe(timeTwo, minusOne)(2)
 
     expect(result).to.equal(expected)
   })
 
-  it('should run compose functions and compue value', () => {
-    const expected = 5
-    const result = compose(addTwo, minusOne, timeTwo)(2)
+  it('should run functions and compue value', () => {
+    const expected = 6
+    const result = pipe(addTwo, minusOne, timeTwo)(2)
 
     expect(result).to.equal(expected)
   })
 
   it('should work with one function', () => {
     const expected = 4
-    const result = compose(addTwo)(2)
+    const result = pipe(addTwo)(2)
 
     expect(result).to.equal(expected)
   })
