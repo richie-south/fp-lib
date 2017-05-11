3c32f23d0dbbdd48ab35a11efabab383f2fdb4c2
rs222kn <rs222kn@student.lnu.se>
2017-05-11 12:35:15 +0200

    added early return

--- src/utils/walk.js
+++ src/utils/walk.js
@@ -8,20 +8,18 @@ const curry = require('../curry')
  * @param {Any} initial - initial value for fn
  * @return {Array||string} _ - collection change by fn
  */
-const walkArray = curry((_fn, fn, array, initial = []) => {
+const walkArray = curry((_fn, fn, array, initial) => {
   const arrayLength = array.length
 
-  const walk = (result = [], index = 0) => {
-    if(arrayLength <= index){
+  const walk = (result, index = 0) => {
+    if (arrayLength <= index) {
       return result
     }
 
     const element = array[index]
-    
-    return walk(
-      _fn(fn, element, result, index), 
-      index + 1
-    )
+    const [value, doEarlyreturn] = _fn(fn, element, result, index)
+
+    return doEarlyreturn ? value : walk(value, index + 1)
   }
   return walk(initial)
 })
@@ -34,22 +32,20 @@ const walkArray = curry((_fn, fn, array, initial = []) => {
  * @param {Any} initial - initial value for fn
  * @return {Object} _ - collection change by fn
  */
-const walkObject = curry((_fn, fn, object, initial = {}) => {
+const walkObject = curry((_fn, fn, object, initial) => {
   const keys = Object.keys(object)
   const keysLength = keys.length
 
-  const walk = (result = {}, index = 0) => {
-    if(keysLength <= index){
+  const walk = (result, index = 0) => {
+    if (keysLength <= index) {
       return result
     }
-    
+
     const key = keys[index]
     const value = object[keys[index]]
+    const [_value, doEarlyreturn] = _fn(fn, value, key, result, index)
 
-    return walk(
-      _fn(fn, value, key, result, index),
-      index + 1
-    )
+    return doEarlyreturn ? _value : walk(_value, index + 1)
   }
 
   return walk(initial)
