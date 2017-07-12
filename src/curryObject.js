93785541e18e14e414d9a6887b590d2867a21c1f
richie-south <richard.soderman@gmail.com>
2017-07-12 22:00:51 +0200

    - comments on curryObject
    - fn is now also curried

--- src/curryObject.js
+++ src/curryObject.js
@@ -2,7 +2,13 @@ const { isArray } = require('./isType')
 const { walkObject, walkArray } = require('./utils/walk')
 const curry = require('./curry')
 
-const doMatchObject = (template, target) => 
+/**
+ * Matches array tamplate with target keys
+ * @param {Array} tamplate - object of keys to fufill
+ * @param {Object} target - object to match keys with
+ * @returns {boolean} _ - full match or not
+ */
+const matchObject = (template, target) => 
   walkObject((_, __, key) => 
     !target.hasOwnProperty(key) 
       ? [false, true]
@@ -12,7 +18,13 @@ const doMatchObject = (template, target) =>
     true
   )
 
-const doMatchArray = (keys, target) => 
+/**
+ * Matches array keys with target jeys
+ * @param {Array} keys - array of keys to fufill
+ * @param {Object} target - object to match keys with
+ * @returns {boolean} _ - full match or not
+ */
+const matchArray = (keys, target) => 
   walkArray((_, key) => 
     !target.hasOwnProperty(key) 
       ? [false, true]
@@ -22,18 +34,28 @@ const doMatchArray = (keys, target) =>
     true
   )
 
-const curryObject = curry((template, fn, curryArgs = {}) => (args) => {
+/**
+ * Simple curryObject with template
+ * @param {object|array} template - template that holds params to fufill
+ * @param {Function} fn - fn to curry
+ * @param {object} curryArgs - first args 
+ * @returns {Function} _ - fn to take rest of the args
+ * @param {object} args - rest or some of the args
+ * @returns {any|Function} _ - eather result of curried fn or curryObject
+ */
+const curryObject = curry((template, fn, curryArgs = {}) => (args = {}) => {
   const concatedArgs = Object.assign({}, curryArgs, args)
+  const _fn = curry(fn)
+ 
   if(isArray(template)) {
-
-    return doMatchArray(template, concatedArgs) 
-      ? fn(concatedArgs)
-      : curryObject(template, fn, concatedArgs) 
+    return matchArray(template, concatedArgs) 
+      ? _fn(concatedArgs)
+      : curryObject(template, _fn, concatedArgs) 
   }
-  
-  return doMatchObject(template, concatedArgs) 
-    ? fn(concatedArgs)
-    : curryObject(template, fn, concatedArgs)
+
+  return matchObject(template, concatedArgs) 
+    ? _fn(concatedArgs)
+    : curryObject(template, _fn, concatedArgs)
 })
 
 module.exports = curryObject
